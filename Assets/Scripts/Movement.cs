a7ae36fdc2d89d698ce5e050b58148e650708a10
iman-jalilvand <63774065+iman-jalilvand@users.noreply.github.com>
2025-01-27 12:08:54 -0800

    Added RigidBody, MoveForce, and TurnTorque Mechanisms

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float MovementSpeed = 80.0f;
+    public float MoveSpeed;
+    public float TurnTorque;
+    public Rigidbody rocket;
+
+    // public float MovementSpeed = 80.0f;
     public float horizontal;
     public float vertical;
 
@@ -17,29 +21,36 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * Time.deltaTime);    
+            // transform.Translate(Vector3.forward * Time.deltaTime);    
+            rocket.AddForce(transform.forward * MoveSpeed);
         }
         
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(-1 * Vector3.forward * Time.deltaTime);    
+           // transform.Translate(-1 * Vector3.forward * Time.deltaTime);    
+            rocket.AddForce(-1 * transform.forward * MoveSpeed);
+
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * Time.deltaTime);    
+            // transform.Translate(Vector3.right * Time.deltaTime);    
+            rocket.AddForce(transform.right * MoveSpeed);
+
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-1 * Vector3.right * Time.deltaTime);    
+            // transform.Translate(-1 * Vector3.right * Time.deltaTime);    
+            rocket.AddForce(-1 * transform.right * MoveSpeed);
+
         }
 
         horizontal = Input.GetAxis("Mouse X");
         vertical = Input.GetAxis("Mouse Y");
 
-        transform.Rotate(Vector3.up, horizontal * Time.deltaTime * MovementSpeed);
-        transform.Rotate(-1 * Vector3.right, vertical * Time.deltaTime * MovementSpeed);
-
+        // transform.Rotate(Vector3.up, horizontal * Time.deltaTime * MovementSpeed);
+        // transform.Rotate(-1 * Vector3.right, vertical * Time.deltaTime * MovementSpeed);
+        rocket.AddRelativeTorque(-1 * vertical * TurnTorque, horizontal * TurnTorque, 0);
     }
 }
